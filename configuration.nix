d11bb7010ab05f58419125f58796374ff665fd0e
Simon Janlöv <simon.janlov@xlnaudio.com>
2025-04-04 15:20:08 +0200

    Added submodules nixpkgs and home-manager, importing home manager via modules/default.nix

--- configuration.nix
+++ configuration.nix
@@ -9,7 +9,6 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
       ./modules
-      <home-manager/nixos>
     ];
 
   simon.gnome.enable = true;
--- modules/default.nix
+++ modules/default.nix
@@ -6,5 +6,6 @@
       ./gnome.nix
       ./emacs.nix
       ./cachix.nix
+      ./home-manager/nixos
     ];
 }
