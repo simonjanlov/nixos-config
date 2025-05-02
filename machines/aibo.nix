b7c2708002f33e1e69a05b9717ee495c3972045d
Simon Janlöv <simon.janlov@gmail.com>
2025-05-02 18:10:06 +0200

    changed the structure of the repo to include a machines folder. Merged hardware-config into the main config, now named aibo.nix

--- machines/aibo.nix
+++ machines/aibo.nix
@@ -1,16 +1,17 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, modulesPath, ... }:
 
 {
   nix.nixPath = [
     "nixpkgs=/etc/nixos/modules/nixpkgs"
-    "nixos-config=/etc/nixos/configuration.nix"
+    "nixos-config=/etc/nixos/machines/aibo.nix"
     "/nix/var/nix/profiles/per-user/root/channels"
   ];
 
   imports =
     [
-      ./hardware-configuration.nix
-      ./modules
+      # ./hardware-configuration.nix
+      (modulesPath + "/installer/scan/not-detected.nix") # why this?
+      ../modules
     ];
 
   simon.gnome.enable = true;
@@ -36,7 +37,7 @@
   hardware.graphics.enable = true;
   hardware.enableRedistributableFirmware = true;
 
-  networking.hostName = "simon-nixos";
+  networking.hostName = "aibo";
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
@@ -95,7 +96,7 @@
     };
 
     home.file.".myconfig".text = ''
-      # This is my custom config file
+      # This is my new custom config file
       export SIMON_MY_VARIABLE="Hello, Home Manager!"
       '';
 
@@ -184,5 +185,47 @@
   hardware.sane.netConf = "printer.internal.xlnaudio.com";
 
   system.stateVersion = "24.11";
+
+
+  ### HARDWARE CONFIGURATION ###
+
+  boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod" ];
+  boot.initrd.kernelModules = [ "dm-snapshot" ];
+  boot.kernelModules = [ "kvm-amd" ];
+  boot.extraModulePackages = [ ];
+
+  boot.initrd.luks.devices.cryptroot.device = "/dev/disk/by-uuid/0713e632-7b7d-4c7e-b0b9-3871c0033b0d";
+
+  services.xserver.videoDrivers = [
+    "modesetting"
+  ];
+
+  fileSystems."/" =
+    { device = "/dev/root_vg/root";
+      fsType = "btrfs";
+      options = [ "subvol=root" ];
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/B831-0FC2";
+      fsType = "vfat";
+      options = [ "fmask=0022" "dmask=0022" ];
+    };
+
+  swapDevices =
+    [ { device = "/dev/root_vg/swap"; }
+    ];
+
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp0s31f6.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlp0s20f3.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
 
   }
