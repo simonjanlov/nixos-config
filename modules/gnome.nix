6d996d25f17fddb4ff24419de23469dafebc57bb
simonjanlov <simon.janlov@gmail.com>
2025-07-30 13:36:49 +0200

    gnome.nix, reverted to including the 'xserver' part of some option names. The stable nixpkgs doesn't seem to accept the new name

--- modules/gnome.nix
+++ modules/gnome.nix
@@ -46,8 +46,8 @@ in
 
 
       # Enable the GNOME Desktop Environment.
-      services.displayManager.gdm.enable = true;
-      services.desktopManager.gnome.enable = true;
+      services.xserver.displayManager.gdm.enable = true;
+      services.xserver.desktopManager.gnome.enable = true;
 
 
       # Configure keymap in X11
