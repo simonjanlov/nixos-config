9a523a1771e8b26abe28d532396931f80116f38b
Simon Janlöv <simon.janlov@gmail.com>
2025-09-29 13:30:28 +0200

    gnome: changed deprecated syntax

--- modules/gnome.nix
+++ modules/gnome.nix
@@ -47,13 +47,8 @@ in
 
 
       # Enable the GNOME Desktop Environment.
-      services.xserver.displayManager.gdm.enable = true;
-      services.xserver.desktopManager.gnome.enable = true;
-
-      # # New syntax from 25.11. Disable gnome on kumo server when
-      # # implementing this.
-      # services.displayManager.gdm.enable = true;
-      # services.desktopManager.gnome.enable = true;
+      services.displayManager.gdm.enable = true;
+      services.desktopManager.gnome.enable = true;
 
 
       # Configure keymap in X11
