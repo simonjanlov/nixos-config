034fdc2dde00ae0ac71c109569accdd1259ce23c
Simon Janlöv <simon.janlov@gmail.com>
2025-11-04 11:09:11 +0100

    vaultwarden: SMTP configuration

--- modules/vaultwarden.nix
+++ modules/vaultwarden.nix
@@ -20,22 +20,22 @@ in
       services.vaultwarden = {
         enable = true;
         config = {
-            ROCKET_ADDRESS = "127.0.0.1";
-            ROCKET_PORT = 8222;
-            ROCKET_WORKERS = 10;
-            SIGNUPS_ALLOWED = false;
-            DOMAIN = "https://vw.${config.simon.domain.homelab.domain}";
-            # SMTP_HOST = "smtp.gmail.com";
-            # SMTP_PORT = 465;
-            # SMTP_SECURITY = "force_tls";
-            # SMTP_USERNAME = "simon.janlov@gmail.com";
-            # SMTP_FROM = "simon.janlov@gmail.com";
-            # SMTP_FROM_NAME = "Vaultwarden";
-            # SMTP_SSL = true;
-            # SMTP_EXPLICIT_TLS = true;
-            PASSWORD_ITERATIONS = 2000000;
-          };
-          environmentFile = keys.vaultwarden-env.path;
+          ROCKET_ADDRESS = "127.0.0.1";
+          ROCKET_PORT = 8222;
+          ROCKET_WORKERS = 10;
+          SIGNUPS_ALLOWED = false;
+          DOMAIN = "https://vw.${config.simon.domain.homelab.domain}";
+          SMTP_HOST = "smtp.gmail.com";
+          SMTP_PORT = 465;
+          SMTP_SECURITY = "force_tls";
+          SMTP_USERNAME = "simon.janlov@gmail.com";
+          SMTP_FROM = "simon.janlov@gmail.com";
+          SMTP_FROM_NAME = "Vaultwarden";
+          SMTP_SSL = true;
+          SMTP_EXPLICIT_TLS = true;
+          PASSWORD_ITERATIONS = 2000000;
+        };
+        environmentFile = keys.vaultwarden-env.path;
         backupDir = "/var/lib/backup-vaultwarden";
       };
 
