4ceb3a3fbd24476602089bc818e689d66fe37718
Simon Janlöv <simon.janlov@gmail.com>
2025-12-08 08:54:23 +0100

    home-assistant: new module for home assistant configuration

---
+++ modules/home-assistant.nix
@@ -0,0 +1,56 @@
+{ config, lib, ... }:
+let
+  cfg = config.simon.home-assistant;
+in
+{
+  options.simon.home-assistant =
+    {
+      enable = lib.mkOption {
+        default = false;
+        example = true;
+        description = ''
+          Whether to host the Home Assistant service
+          '';
+      };
+    };
+
+  config = lib.mkIf cfg.enable
+    {
+      virtualisation.oci-containers.containers = {
+        home-assistant = {
+          environment.TZ = config.time.timeZone;
+          image = "ghcr.io/home-assistant/home-assistant:stable";
+          extraOptions = [
+            "--network=host"
+            "--device=/dev/ttyUSB0:/dev/ttyUSB0"
+          ];
+          volumes = [
+            "home-assistant:/config"
+            "/run/dbus:/run/dbus:ro"
+          ];
+        };
+      };
+
+      simon.backups.paths = [ "/var/lib/containers" ];
+
+      simon.nginx-base.enable = true;
+
+      services.nginx.virtualHosts =
+        {
+          "home.${config.simon.domain.homelab.domain}" =
+            {
+              forceSSL = true;
+              enableACME = true;
+
+              extraConfig = ''
+                proxy_buffering off;
+                '';
+
+              locations."/" = {
+                proxyPass = "http://127.0.0.1:8123";
+                proxyWebsockets = true;
+              };
+            };
+        };
+    };
+}
