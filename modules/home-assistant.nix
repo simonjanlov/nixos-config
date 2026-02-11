f4e5b8c40ce7e420d642fb0004e42dcd8b35f780
Simon Janlöv <simon.janlov@gmail.com>
2026-02-11 15:12:17 +0100

    chore: updates home-assistant image version

--- modules/home-assistant.nix
+++ modules/home-assistant.nix
@@ -19,7 +19,7 @@ in
       virtualisation.oci-containers.containers = {
         home-assistant = {
           environment.TZ = config.time.timeZone;
-          image = "ghcr.io/home-assistant/home-assistant:2025.12.2";
+          image = "ghcr.io/home-assistant/home-assistant:2026.2.1";
           extraOptions = [
             "--network=host"
             "--device=/dev/ttyUSB0:/dev/ttyUSB0"
