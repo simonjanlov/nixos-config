eb85541a7f575b76462fe7d1bbc2dedbd7577a5c
Simon Janlöv <simon.janlov@gmail.com>
2026-02-27 15:45:52 +0100

    backups: includes postgresql backups to restic

--- modules/backups.nix
+++ modules/backups.nix
@@ -5,6 +5,8 @@ let
 
   inherit (lib)
     mkOption
+    mkMerge
+    mkIf
     types;
 in
 {
@@ -37,47 +39,68 @@ in
           What time to run the backup.
         '';
       };
+      enablePostgresqlBackup = mkOption {
+        default = config.services.postgresql.enable;
+        type = types.bool;
+        description = ''
+          Whether to backup PostgreSQL database.
+        '';
+      };
+      includePostgresqlBackup = mkOption {
+        default = cfg.enablePostgresqlBackup;
+        type = types.bool;
+        description = ''
+          Whether to add the PostgreSQL backup location to the list of backup paths.
+        '';
       };
     };
 
-  config = lib.mkIf cfg.enable
-    {
-      services.restic.backups = {
-        gdrive = lib.mkMerge [
-          {
-            exclude = cfg.exclude;
-            initialize = true;
-            paths = cfg.paths;
-            pruneOpts = [
-              "--keep-daily 7"
-              "--keep-weekly 5"
-              "--keep-monthly 12"
-              "--keep-yearly 3"
-            ];
-            rcloneOptions = {
-              checksum = true;
-              transfers = "30";
-            };
-            repository = "rclone:gdrive:${config.networking.hostName}";
-            # runCheck = true;
-            # checkOpts = [ "--read-data-subset=10%" ];
-            timerConfig = {
-              OnCalendar = cfg.time;
-              # OnUnitActiveSec = "300s"; # For testing
-            };
-          }
+  config = mkMerge [
+    (mkIf cfg.enable {
+      services.restic.backups =
+        {
+          gdrive = mkMerge [
+            {
+              exclude = cfg.exclude;
+              initialize = true;
+              paths = cfg.paths;
+              pruneOpts = [
+                "--keep-daily 7"
+                "--keep-weekly 5"
+                "--keep-monthly 12"
+                "--keep-yearly 3"
+              ];
+              rcloneOptions = {
+                checksum = true;
+                transfers = "30";
+              };
+              repository = "rclone:gdrive:${config.networking.hostName}";
+              # runCheck = true;
+              # checkOpts = [ "--read-data-subset=10%" ];
+              timerConfig = {
+                OnCalendar = cfg.time;
+              };
+            }
+            # If current system is the deployment host
+            (mkIf config.simon.deployment-tools.enable {
+              passwordFile = "/home/simon/.keys/restic-password";
+              rcloneConfigFile = "/home/simon/.config/rclone/rclone.conf";
+            })
+            # Else
+            (mkIf (!config.simon.deployment-tools.enable) {
+              passwordFile = config.deployment.keys.restic-password.path;
+              rcloneConfigFile = config.deployment.keys."rclone.conf".path;
+            })
+          ];
+        };
+    })
 
-          # If current system is the deployment host
-          (lib.mkIf config.simon.deployment-tools.enable {
-            passwordFile = "/home/simon/.keys/restic-password";
-            rcloneConfigFile = "/home/simon/.config/rclone/rclone.conf";
-          })
-          # Else
-          (lib.mkIf (!config.simon.deployment-tools.enable) {
-            passwordFile = config.deployment.keys.restic-password.path;
-            rcloneConfigFile = config.deployment.keys."rclone.conf".path;
-          })
-        ];
-      };
-    };
+    (mkIf cfg.enablePostgresqlBackup {
+      services.postgresqlBackup.enable = true;
+    })
+
+    (mkIf cfg.includePostgresqlBackup {
+      simon.backups.paths = [ "${config.services.postgresqlBackup.location}" ];
+    })
+  ];
 }
