928193d07f3fa1ff47fafe6f5b854b1fe2eab8e2
Simon Janlöv <simon.janlov@gmail.com>
2026-04-16 13:41:28 +0200

    prompt-styling: comments out powerline-go configuration

--- modules/prompt-styling.nix
+++ modules/prompt-styling.nix
@@ -22,25 +22,27 @@ in
       # };
 
 
-      environment.systemPackages = [ pkgs.powerline-go ];
+      ### POWERLINE-GO HOME MANAGER MANUAL ###
 
-      home-manager.users.${config.simon.username} = {
-        programs.bash.initExtra = ''
-          function _update_ps1() {
-            PS1="$(${pkgs.powerline-go}/bin/powerline-go -error $? -jobs $(jobs -p | wc -l) -newline -cwd-mode "fancy" -git-mode "compact")"
+      # environment.systemPackages = [ pkgs.powerline-go ];
 
-            # Uncomment the following line to automatically clear errors after showing
-            # them once. This not only clears the error for powerline-go, but also for
-            # everything else you run in that shell. Don't enable this if you're not
-            # sure this is what you want.
+      # home-manager.users.${config.simon.username} = {
+      #   programs.bash.initExtra = ''
+      #     function _update_ps1() {
+      #       PS1="$(${pkgs.powerline-go}/bin/powerline-go -error $? -jobs $(jobs -p | wc -l) -newline -cwd-mode "fancy" -git-mode "compact")"
 
-            #set "?"
-          }
+      #       # Uncomment the following line to automatically clear errors after showing
+      #       # them once. This not only clears the error for powerline-go, but also for
+      #       # everything else you run in that shell. Don't enable this if you're not
+      #       # sure this is what you want.
 
-          if [ "$TERM" != "linux" ]; then
-            PROMPT_COMMAND="_update_ps1; $PROMPT_COMMAND"
-          fi
-        '';
-      };
+      #       #set "?"
+      #     }
+
+      #     if [ "$TERM" != "linux" ]; then
+      #       PROMPT_COMMAND="_update_ps1; $PROMPT_COMMAND"
+      #     fi
+      #   '';
+      # };
     };
 }
