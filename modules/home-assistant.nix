83ba9f6b18c4127bab790a5b880a5f98a1a997c0
Simon Janlöv <simon.janlov@gmail.com>
2026-09-11 15:52:05 +0200

    home-assistant: bumps container image version

--- modules/home-assistant.nix
+++ modules/home-assistant.nix
@@ -19,7 +19,7 @@ in
       virtualisation.oci-containers.containers = {
         home-assistant = {
           environment.TZ = config.time.timeZone;
-          image = "ghcr.io/home-assistant/home-assistant:2026.5.1";
+          image = "ghcr.io/home-assistant/home-assistant:2026.8.3";
           extraOptions = [
             "--network=host"
             "--device=/dev/ttyUSB0:/dev/ttyUSB0"
